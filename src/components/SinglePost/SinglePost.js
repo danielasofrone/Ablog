6e8f0f2c60270daf77dc795390911018d3816d7e
Daniela <daniela.sofrone.sd@gmail.com>
2020-11-02 13:45:18 +0100

    added back button in single post page

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import * as S from "./singlePost.styled";
+import * as C from "./singlePost.styled";
+import * as S from '../BlogPost/blogPost.styled'
 import NavBar from "../NavBar/NavBar";
-import BlogPost from "../BlogPost/BlogPost";
 import client from "../../client";
+import { Link } from "react-router-dom";
 
 const SinglePost = () => {
   const [singleBlogPost, setSingleBlogPost] = useState(null);
@@ -23,7 +24,7 @@ const SinglePost = () => {
   }, [slug]);
 
   return (
-    <S.Wrapper>
+    <C.PageContainer>
       {loading ? (
         <div>
           <h2>Loading...</h2>
@@ -31,15 +32,26 @@ const SinglePost = () => {
       ) : (
         <div>
           <NavBar />
-          <BlogPost
-            title={singleBlogPost.fields.title}
-            text={singleBlogPost.fields.text}
-            coverImage={singleBlogPost.fields.picture.fields.file.url}
-          />
+          <C.BackButtonContainer>
+            <C.BackButton>
+              <Link to="/">&larr; go back</Link>
+            </C.BackButton>
+          </C.BackButtonContainer>
+          <S.Wrapper>
+            <C.SinglePostTitle>
+              {singleBlogPost.fields.title}
+            </C.SinglePostTitle>
+            <S.PostText>{singleBlogPost.fields.text}</S.PostText>
+            </S.Wrapper>
         </div>
       )}
-    </S.Wrapper>
+    </C.PageContainer>
   );
 };
 
 export default SinglePost;
+
+            // title={singleBlogPost.fields.title}
+            // text={singleBlogPost.fields.text}
+            // coverImage={singleBlogPost.fields.text}
+    
--- src/components/SinglePost/singlePost.styled.js
+++ src/components/SinglePost/singlePost.styled.js
@@ -1,5 +1,50 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const PageContainer = styled.div`
   margin: 0 10%;
+`;
+
+export const SinglePostTitle = styled.div`
+  font-family: "Questrial", sans-serif;
+  font-size: 1.125rem;
+  text-align: center;
+  margin-bottom: 30px;
+  a {
+    color: rgb(65, 65, 65);
+    text-decoration: none;
+    &:hover {
+      color: #000;
+    }
+  }
+  @media (min-width: 768px) {
+    font-size: 1.5rem;
+    margin-bottom: 35px;
+  }
+  @media (min-width: 768px) {
+    font-size: 2rem;
+    margin-bottom: 40px;
+  }
+`;
+
+export const BackButtonContainer = styled.div`
+ float: left;
+ margin: 30px 0;
+ display: flex;
+ flex-direction: row;
+`;
+
+export const BackButton = styled.div`
+ background-color: #fff;
+ font-family: "Questrial", sans-serif;
+  font-size: 1.125rem;
+  text-align: center;
+  padding: 5px;
+  cursor: pointer;
+  a {
+    color: rgb(65, 65, 65);
+    text-decoration: none;
+    &:hover {
+      color: #000;
+    }
+  }
 `;
